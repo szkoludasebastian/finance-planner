5af20f2e1c72ba739656d60fa6055d65f3bb6f36
Sebastian Szkoluda <sebastian.szkoluda@infor.com>
2026-03-21 14:10:41 +0100

    Style side menu

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3567189968" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2755257769" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
